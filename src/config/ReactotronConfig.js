5e13cbc4c407092a5c74d98357717a7349c94331
Vinícius Bastos <viniciusbsilva57@gmail.com>
2024-03-02 01:03:05 -0300

    fix - alteração no ip do ReactoTron

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -2,7 +2,7 @@ import Reactotron from 'reactotron-react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
 Reactotron.setAsyncStorageHandler(AsyncStorage)
-  .configure({host: '192.168.15.128'}) // controls connection & communication settings
+  .configure({host: '192.168.0.107'}) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .connect(); // let's connect!
 
